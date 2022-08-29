b196f7d5ee8b1f727966462405969c4e0720247a
dinamaged1 <76622624+dinamaged1@users.noreply.github.com>
2022-08-29 04:12:56 +0200

    Resize images before save it done & Fixed bugs

--- RecipeClient/RecipeClient/Pages/Index.cshtml.cs
+++ RecipeClient/RecipeClient/Pages/Index.cshtml.cs
@@ -1,10 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.Processing;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web;
 
 
 namespace RecipeClient.Pages
 {
-    public class Image
+    public class ImageData
     {
         public string FileName { get; set; } = string.Empty;
         public string Base64String { get; set; }= string.Empty;
@@ -15,6 +22,7 @@ namespace RecipeClient.Pages
     {
         private readonly ILogger<IndexModel> _logger;
         private readonly IWebHostEnvironment _host;
+        public static Configuration? Default { get; }
         [BindProperty]
         public string ApiUrl { get; set; }
 
@@ -25,18 +33,47 @@ namespace RecipeClient.Pages
             ApiUrl = config.GetRequiredSection("url").Get<string>();
             _host = host;
         }
-
-        public async Task OnPostSaveImageToFolder([FromBody] Image recievedImage)
+        public async Task OnPostSaveImageToFolder([FromBody] ImageData recievedImage)
         {
             byte[] imageBytes = Convert.FromBase64String(recievedImage.Base64String);
-            MemoryStream ms = new MemoryStream(imageBytes, 0,
+            MemoryStream msOriginalImage = new MemoryStream(imageBytes, 0,
               imageBytes.Length);
-            var data = ms.ToArray();
+
+            IImageFormat format;
+            MemoryStream msResizedImage = new MemoryStream();
+            using (Image image = Image.Load(msOriginalImage, out format))
+            {
+                List<int> resizingData = ResizeImage(image, 700, 700);
+                image.Mutate(x => x.Resize(resizingData[0], resizingData[1]));
+                image.Save(msResizedImage, format);
+            }
+            var data= msResizedImage.ToArray();
             var filePath = $"{_host.WebRootPath}/RecipesImages/{recievedImage.FileName}";
             await System.IO.File.WriteAllBytesAsync(filePath, data);
         }
 
-        public async Task OnPostDeleteImageFolder([FromBody] Image recievedImage)
+        public List<int> ResizeImage(SixLabors.ImageSharp.Image img,int maxWidth,int maxHeight)
+        {
+            List<int> resizingData = new List<int>();
+            if(img.Width != maxWidth || img.Height != maxHeight)
+            {
+                double widthRatio=(double)img.Width / (double)maxWidth;
+                double heightRatio=(double)img.Height / (double)maxHeight;
+                double ratio=Math.Max(heightRatio, widthRatio);
+                int newWidth=(int)(img.Width / ratio);
+                int newHeight=(int)(img.Height / ratio);
+                resizingData.Add(newWidth);
+                resizingData.Add(newHeight);
+            }
+            else
+            {
+                resizingData.Add(img.Width);
+                resizingData.Add(img.Height);    
+            }
+            return resizingData;
+        }
+
+        public void OnPostDeleteImageFolder([FromBody] ImageData recievedImage)
         {
             //Delete Image from folder RecipeImages
             var filePathDelete = $"{_host.WebRootPath}{recievedImage.FileName}";
